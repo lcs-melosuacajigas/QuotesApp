5b88e31597f0e5c693e3561d7e56f0abeb2bdb13
Mateo Elosua <melosua@lcs.on.ca>
2022-02-24 12:23:15 -0500

    Initial Commit

---
+++ QuotesApp/QuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAppApp.swift
+//  QuotesApp
+//
+//  Created by Mateo Elosua on 2022-02-24.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
